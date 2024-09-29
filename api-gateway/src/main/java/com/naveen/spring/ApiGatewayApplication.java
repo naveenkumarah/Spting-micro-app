6cd8f23b5926e57cc0c08436a7060be71183e701
naveenkumarah82 <131598256+naveenkumarah82@users.noreply.github.com>
2024-09-29 21:02:58 +0530

    keyclock integration with api-gateway

--- api-gateway/src/main/java/com/naveen/spring/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/naveen/spring/ApiGatewayApplication.java
@@ -1,13 +1,30 @@
 package com.naveen.spring;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.WebSession;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@RestController
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class);
+    }
+
+    @GetMapping(value = "/token")
+    public Mono<String> getHome(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+        return Mono.just(authorizedClient.getAccessToken().getTokenValue());
+    }
+
+    @GetMapping("/")
+   // @PreAuthorize("hasAuthority('product_read')")
+    public Mono<String> index(WebSession session) {
+                    return Mono.just(session.getId());
     }
 }
---
+++ api-gateway/src/main/java/com/naveen/spring/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.naveen.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(auth -> auth.anyExchange().authenticated())
+                .oauth2Login(withDefaults())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return http.build();
+    }
+
+}
